574ba7976177559492fca162c685b06d90a2b818
Berraiz <jfsnow00@gmail.com>
2024-10-07 19:37:07 -0700

    final

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -9,12 +9,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXfxsEw9NnwKAbXgpY54YBSW4nvKEajRw",
-  authDomain: "socialnetworkproject-e0599.firebaseapp.com",
-  projectId: "socialnetworkproject-e0599",
-  storageBucket: "socialnetworkproject-e0599.appspot.com",
-  messagingSenderId: "25609857269",
-  appId: "1:25609857269:web:a6d52ff12f323ff5bf3dbc"
+  apiKey: "AIzaSyB5lQs0rUc5ljSvlFxRUamwcpScQrQ-Nv0",
+  authDomain: "portfolio-v1-joaoneves.firebaseapp.com",
+  projectId: "portfolio-v1-joaoneves",
+  storageBucket: "portfolio-v1-joaoneves.appspot.com",
+  messagingSenderId: "1006755374363",
+  appId: "1:1006755374363:web:e7233c606c995af6cd1fbe"
 };
 
 
